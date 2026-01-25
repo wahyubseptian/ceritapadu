478559110af5594cf922e60d127a7676a94bcd83
Wahyu B Septian <68914282+wahyubseptian@users.noreply.github.com>
2026-01-25 22:24:31 +0700

    Add files via upload

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "766760667"
+    serviceWorkerVersion: "2687922952"
   }
 });
